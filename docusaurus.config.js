8dd1fbd456c4acb546e35ef97a704cb3ed440a04
Priyanshu Dangi <priyanshudangi37@gmail.com>
2022-03-01 00:57:07 +0530

    fix: overview.md to index.md

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,7 +52,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'overview',
+            docId: 'index',
             position: 'left',
             label: 'Tutorial',
           },
